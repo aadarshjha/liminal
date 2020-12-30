fec7fac431e0701112e1408a80eb401dc3de8dd6
aadarshjha <jha.aadarsh@gmail.com>
2020-12-29 18:50:04 -0600

    fixed css issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,16 @@
 <script>
 	import Header from "./layout/Header.svelte";
 	import marked from "../node_modules/marked/marked.min.js"
+	import "../public/style/main.css"; 
 	import { onMount } from "svelte";
-	// import "../node_modules/github-markdown-css/github-markdown.css"
-
-	onMount(async () => {
-		document.getElementById("content").innerHTML = marked("# Marked in the browser\n\nRendered by **marked**. \n <code>test this is a test that i smore code etcthat eixsts siasdj;flads lkf sadklfj ;laskdf asfjkls;fjs;kfj ;lskj f;ljs df;ljs ;lkfj ;sdlkj f;laksj df;klasj fdkjas f;kjsa kjf s;dklfj ;klj</code>");
-	}); 
 
 </script>
 
-<style>
-	#content h1 {
-		color: red; 
-		background-color: red; 
-		font-size: 100x;
-	}
-</style>
 
+<Header />
+<!-- <div id = "content">
 
-<main>
-	<Header />
-	<div id = "content">
-
-	</div>
-
-</main>
+</div> -->
 
 
 
--- src/layout/Header.svelte
+++ src/layout/Header.svelte
@@ -1,9 +1,20 @@
 <style>
-
+    /* h1 {
+        color: red; 
+    } */
 </style>
 
 <script>
+    import marked from "../../node_modules/marked/marked.min.js"
+    import { onMount } from "svelte";
+    onMount(async () => {
+		document.getElementById("content").innerHTML = marked("Marked in the browser\n\nRendered by **marked**. \n <code>test this is a test that i smore code etcthat eixsts siasdj;flads lkf sadklfj ;laskdf asfjkls;fjs;kfj ;lskj f;ljs df;ljs ;lkfj ;sdlkj f;laksj df;klasj fdkjas f;kjsa kjf s;dklfj ;klj</code>");
+	}); 
 
 </script>
 
 <h1>Test</h1>
+
+<div id="content">
+
+</div>
