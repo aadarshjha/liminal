9d6bebfab7f4fa3887b8be1db8cd3b37b08eb0a9
aadarshjha <jha.aadarsh@gmail.com>
2020-12-30 02:04:20 -0600

    json data so no api

--- src/layout/Header.svelte
+++ src/layout/Header.svelte
@@ -15,15 +15,15 @@
 
 <script>
     import marked from "../../node_modules/marked/marked.min.js"
+    import emojis from "../json/emojis.json"; 
     import { onMount } from "svelte";
-    import axios from "../../node_modules/axios/dist/axios.js"; 
 
     onMount(async () => {
 		document.getElementById("content").innerHTML = marked("Marked in the browser\n\nRendered by **marked**. \n <code>test this is a test that i smore code etcthat eixsts siasdj;flads lkf sadklfj ;laskdf asfjkls;fjs;kfj ;lskj f;ljs df;ljs ;lkfj ;sdlkj f;laksj df;klasj fdkjas f;kjsa kjf s;dklfj ;klj</code>");
     }); 
 
-    let potentialEmojis = ["res/1.png", "res/2.png", "res/3.png", "res/4.png"];
-    // let person = ;
+    console.log(emojis); 
+
 </script>
 
 <div class = "random-gen">
