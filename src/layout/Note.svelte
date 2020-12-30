90f4e4cd37f5684c4817830ec4ad2e491804a752
aadarshjha <jha.aadarsh@gmail.com>
2020-12-30 03:39:29 -0600

    imges look fine,need to work on code

--- src/layout/Note.svelte
+++ src/layout/Note.svelte
@@ -21,25 +21,18 @@
         margin-top: 0px;
         color: rgb(160,160,160); 
     }
-    .meta {
-        /* background-color: red;  */
-    }
 </style>
 
 <script>
     import marked from "../../node_modules/marked/marked.min.js"
     import { onMount } from "svelte";
     import { nanoid } from '../../node_modules/nanoid/nanoid.js'; 
+    import highlight from "../../node_modules/highlight.js/lib/highlight.js"
     import post from "../json/posts.json"; 
     // console.log(nanoid()); 
     // model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"    
 
     onMount(async () => {
-		// const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=20`);
-        // photos = await res.json();
-        // str = ""
-        // document.getElementById('card').innerHTML = marked('# Marked in the browser \n <img src = "res/moon.png" alt="a photo." /> <p>This is a test this is a setup etasd;fka skfj ;aks </p> \n #### part 3 why croes die <code>hello world a;lksdf ksd laks fjaslfksdklfsd fadsfj dslk;f sflksdlf sdkfja ;lskf ;aksfkl</code>');
-
         // get all the id's 
         for (let fetch of post) {
             let doc = document.getElementById("" + fetch["post_id"]); 
@@ -48,12 +41,7 @@
             doc.innerHTML += marked(fetch['body']); 
         }
 	});
-
-    
-
 </script>
-
-<!-- fetches only from JSON -->
 
 
 {#each post as card}
