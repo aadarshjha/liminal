c4b3723c3cc55ccade0fccce4dc47c9fa879da42
aadarshjha <jha.aadarsh@gmail.com>
2020-12-30 22:57:10 -0600

    working on images

--- src/layout/Imgs.svelte
+++ src/layout/Imgs.svelte
@@ -1,5 +1,13 @@
 <style>
-
+    .images {
+        padding-top: 20px;
+    }
+    .images img {
+        margin-bottom: 10px; 
+    }
+    .gray  {
+        
+    }
 </style>
 
 <script>
@@ -7,7 +15,22 @@
 </script>
 
 
-<div class = "img-grid">
+<div class = "images">
+    <div class = "test">  
+        <img src="./res/imgs/IMG_5277.jpg" alt="">
+        <p><em>Walking home today after eating dinner.</em> | <span class = "gray">December 30, 2020</span></p>
+    </div>
 
-    
+    <div class = "test">  
+        <img src="./res/imgs/IMG_5277.jpg" alt="">
+        <p>Walking home today after eating dinner.</p>
+    </div>
+
+    <div class = "test">  
+        <img src="./res/imgs/IMG_5277.jpg" alt="">
+        <p><em>Walking home today after eating dinner.</em></p>
+    </div>
+
+
+
 </div>
